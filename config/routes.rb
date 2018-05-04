72189c2e8cf57feaeeffbef00387e66b9d59d669
Torajiro Shiihashie <uptempo1996@gmail.com>
2018-05-05 02:43:13 +0900

    5/4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/top' => "home#top"
-  get "posts/index" => "posts#index"
-  get "posts/:id" => "posts#show"
-  get "posts/edit" => "posts#edit"
+  resources :users
+  resources :posts
+  get '/YABOU.' => "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
