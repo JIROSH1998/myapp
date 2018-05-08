6ab2cc51a20eac47c1e8a3c872046bbd9e550f4d
Torajiro Shiihashie <uptempo1996@gmail.com>
2018-05-09 02:58:35 +0900

    DB

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   get '/YABOU.' => "home#top"
-
+  post "posts" => "posts#create"  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
